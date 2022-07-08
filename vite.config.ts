995ff8a356999941c8d9e0434a447d983a13b1f8
Martijn Faassen <faassen@startifact.com>
2022-07-08 09:55:47 +0200

    Try to get the build right

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,40 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import path from "path";
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import typescript from "@rollup/plugin-typescript";
 
 export default defineConfig({
   plugins: [solidPlugin()],
   build: {
-    target: 'esnext',
+    target: "esnext",
     polyfillDynamicImport: false,
+    lib: {
+      entry: path.resolve(__dirname, "src/solid-dexie.ts"),
+      name: "solid-dexie",
+      fileName: (format) => `solid-dexie.${format}.js`,
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["solid-js", "solid-js/store", "dexie"],
+      output: {
+        globals: {
+          "solid-js": "solidJS",
+          "solid-js/store": "solidJSStore",
+          dexie: "dexie",
+        },
+      },
+      plugins: [
+        typescript({
+          target: "es2020",
+          rootDir: "src",
+          emitDeclarationOnly: true,
+          declaration: true,
+          declarationDir: "dist",
+          exclude: ["**/*.test.ts", "**/*.tsx", "**/*fixture.ts"],
+          allowSyntheticDefaultImports: true,
+        }),
+      ],
+    },
   },
 });
