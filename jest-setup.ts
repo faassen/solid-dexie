9fb4ba1b229bb03cf75581c9f771b270e3a4e1d6
Martijn Faassen <faassen@startifact.com>
2022-07-08 10:33:29 +0200

    Simpler test setup

--- jest-setup.ts
+++ jest-setup.ts
@@ -1,3 +1,1 @@
 import "regenerator-runtime/runtime";
-import "@testing-library/jest-dom";
-import "@testing-library/jest-dom/extend-expect";
--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  // These transform patterns do absolutely nothing in making things work
-  // prettier-ignore
-  transformIgnorePatterns: ["/node_modules/(?!(solid-js/web|solid-app-router|dexie)/)"],
+  transformIgnorePatterns: [
+    "/node_modules/(?!(solid-js/web|solid-app-router|dexie)/)",
+  ],
   setupFilesAfterEnv: ["<rootDir>/jest-setup.ts"],
   // uses a webpack style resolver, the default one has many issues.
   // but if I disable it, tests run much faster
